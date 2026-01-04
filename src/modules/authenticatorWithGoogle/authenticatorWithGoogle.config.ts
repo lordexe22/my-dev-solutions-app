abd166d1920b92fa0da6cc0280c77544ac9faa26
lordexe22 <walterezequielpuig@gmail.com>
2026-01-04 19:30:33 -0300

    refactor: Eliminar módulo AuthenticatorWithGoogle y archivos relacionados

--- src/modules/authenticatorWithGoogle/authenticatorWithGoogle.config.ts
+++
@@ -1,6 +0,0 @@
-// authenticatorWithGoogle.config.ts
-import type { GoogleConfig } from "./authenticatorWithGoogle.types";
-
-export const googleConfig: GoogleConfig = {
-  clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-};
--- src/modules/authenticatorWithGoogle/index.ts
+++
@@ -1,4 +0,0 @@
-// src\modules\authenticatorWithGoogle\index.ts
-
-export { default as AuthenticatorWithGoogle } from "./authenticatorWithGoogle";
-export * from "./authenticatorWithGoogle.types";
